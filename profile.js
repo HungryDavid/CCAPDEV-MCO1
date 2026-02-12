357946a6a5e66d52d587c194cb4516198b07b613
jr-yulo <jr.yulo@outlook.com>
2026-02-12 17:21:08 +0800

    added profile feature

--- profile.js
+++ profile.js
@@ -1,1 +1,17 @@
-document.addEventListener()
+const editButton = document.getElementById('edit-btn');
+const area = document.getElementById('description');
+
+editButton.addEventListener('click', () => {
+  const isReadOnly = area.readOnly;
+
+  if (isReadOnly) {
+    area.readOnly = false;
+    area.focus();
+    area.setSelectionRange(area.value.length, area.value.length);
+    editButton.innerHTML = '<i class="fas fa-save" aria-hidden="true"></i><span>Save</span>';
+  } else {
+    area.readOnly = true;
+    editButton.innerHTML = '<i class="fa fa-pencil" aria-hidden="true"></i><span>Edit Profile</span>';
+  }
+
+});
